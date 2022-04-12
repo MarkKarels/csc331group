2ee0d6b74191874a489adc6673507e5dfa898c9b
Mark Karels <mak8966@uncw.edu>
2022-04-12 16:39:08 -0400

    GUI Change

--- src/QB.java
+++ src/QB.java
@@ -37,7 +37,7 @@ public class QB extends Player{
 
     @Override
     public String toString() {
-        return String.format("%n%-20s%-20s%s%n%n%-33s%,.1f pts%n%n%48s%n%n%-32s%,.1f pts%n%-35s%,.1f yds%n%-27s%d tds%n%-29s%d ints%n%-30s%d per%n%-34s%,.1f yds%n%-26s%d tds",
+        return String.format("%n%s%n%s%n%s%n%n%-28s%,.1f pts%n%n%28s%n%-31s%,.1f pts%n%-29s%,.1f yds%n%-28s%d tds%n%-29s%d ints%n%-28s%d per%n%-29s%,.1f yds%n%-27s%d tds",
                 getPosition(),
                 getPlayer(), getTeam(),
                 "2021 Total Points: ", getPrevPoints(),

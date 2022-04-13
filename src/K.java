75615acb7b61b5e9bf815c3f1e4f6b13dca7f683
Mark Karels <100231843+MarkKarels@users.noreply.github.com>
2022-04-13 10:03:26 -0400

    Karels (#3)
    * GUI Change
    
    * Added classes def, flex and K

---
+++ src/K.java
@@ -0,0 +1,9 @@
+public class K {
+    private int made1to39;
+    private int made40to49;
+    private int made50plus;
+    private int totalAttempt;
+    private int totalMade;
+    private int xpAttempt;
+    private int xpMade;
+}
